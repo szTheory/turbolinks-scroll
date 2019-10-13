094e7b9c15936dbb0b02283b78034b18804d5de6
szTheory <szTheory@users.noreply.github.com>
2019-10-12 23:39:47 -0400

    remove debug

--- src/scroll.js
+++ src/scroll.js
@@ -1,7 +1,5 @@
 import delegate from 'delegate'
 
-console.log("----1")
-
 // Based on code from "How To: Turbolinks 5 Scroll Position Persistence" by Sedad Kosovac
 // ref: https://medium.com/@kosovacsedad/how-to-turbolinks-5-scroll-position-persistence-6e4435a60b2e
 class TurbolinksScroller {
@@ -13,16 +11,11 @@ class TurbolinksScroller {
   }
 
   setTop() {
-    console.log("setTop()")
     this.top = this.document.scrollingElement.scrollTop
-    console.log(".... this.top: " + this.top)
   }
 
   scrollToTop() {
-    console.log("scrollToTop()")
-    console.log(".... this.top: " + this.top)
     if (this.top) {
-      console.log("scrollToTop().... this.top was "+this.top)
       this.document.scrollingElement.scrollTo(0, this.top)
     }
     this.top = null
