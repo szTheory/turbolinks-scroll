cf69431407e8325376ed2e97568e4f7b6b2ca282
szTheory <szTheory@users.noreply.github.com>
2019-10-15 11:11:23 -0400

    code cleanup, define constants

--- src/scroll.ts
+++ src/scroll.ts
@@ -3,11 +3,14 @@ import delegate = require("delegate")
 // Based on code from "How To: Turbolinks 5 Scroll Position Persistence" by Sedad Kosovac
 // ref: https://medium.com/@kosovacsedad/how-to-turbolinks-5-scroll-position-persistence-6e4435a60b2e
 class TurbolinksScroller {
-  // top scroll position
-  scrollPosition : number | null
+  DEFAULT_SCROLL_POSITION = null
+  DEFAULT_PERSIST_SCROLL_FOR_NEXT_VISIT = false
 
   // mark the next turbolinks visit to persist scroll
-  persistScrollForNextVisit : boolean
+  persistScrollForNextVisit = this.DEFAULT_PERSIST_SCROLL_FOR_NEXT_VISIT
+
+  // top scroll position
+  scrollPosition: number | null = this.DEFAULT_SCROLL_POSITION
 
   // DOM Document
   document: Document
@@ -15,12 +18,6 @@ class TurbolinksScroller {
   constructor(document : Document) {
     // store the document
     this.document = document
-
-    // default scroll disabled
-    this.persistScrollForNextVisit = false
-    this.scrollPosition = null
-
-    // setup
     this._setup()
   }
 
@@ -32,23 +29,30 @@ class TurbolinksScroller {
       // store the current scroll top position
       this.scrollPosition = this.document.scrollingElement.scrollTop
     } else {
-      throw "Tried to set top for turbolinks-scroll but document.scrollingElement.scrollTop was not set"
+      throw "Tried to set scroll position for turbolinks-scroll but document.scrollingElement.scrollTop was not set"
     }
   }
 
   // scroll to the stored top position if one is saved
   scrollToStoredPosition() {
-    if (this.scrollPosition && this.document.scrollingElement && this.document.scrollingElement.scrollTop !== null) {
+    if (this.scrollPosition && this.document.scrollingElement) {
 
       // scroll page to the stored position
       this.document.scrollingElement.scrollTo(0, this.scrollPosition)
+    } else {
+      throw "Tried scroll to stored position for turbolinks-scroll but document.scrollingElement was not set"
     }
 
+    this._resetPersistence()
+  }
+  
+  // default to scroll disabled
+  _resetPersistence() {
     // reset the stored scroll position
-    this.scrollPosition = null
+    this.persistScrollForNextVisit = this.DEFAULT_PERSIST_SCROLL_FOR_NEXT_VISIT
 
     // reset the persistence flag
-    this.persistScrollForNextVisit = false
+    this.scrollPosition = this.DEFAULT_SCROLL_POSITION
   }
 
   // private setup method
