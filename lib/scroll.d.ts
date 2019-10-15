e139d25ac06a00f02056c3951061af526b0e6285
szTheory <szTheory@users.noreply.github.com>
2019-10-15 10:14:55 -0400

    typescript build

--- lib/scroll.d.ts
+++ lib/scroll.d.ts
@@ -1,2 +1,3 @@
 export declare function turbolinksScrollSetup(document: Document): void;
+export declare function turbolinksPersistScrollForNextVisit(): void;
 export declare function turbolinksScrollSetTop(): void;
