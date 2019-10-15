927abcc1589ac6c0e52e92776a5cace47358d61f
szTheory <szTheory@users.noreply.github.com>
2019-10-15 12:40:04 -0400

    fix typescript bindings

--- lib/scroll.d.ts
+++ lib/scroll.d.ts
@@ -1,3 +1,3 @@
 export declare function turbolinksScrollSetup(document: Document): void;
 export declare function turbolinksPersistScrollForNextVisit(): void;
-export declare function turbolinksScrollSetTop(): void;
+export declare function turbolinksScrollsetScrollPosition(): void;
